ac8a777af28d445da6bf7a8cb540a67861363e65
nikita <nikitachertkov@gmail.com>
2022-09-09 17:37:30 +0500

    MVC + упорядочил мелкие задачи по папкам

---
+++ MVC realisation/model/FakeModel.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model{
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("pidor",12,5));
+        users.add(new User("pidor1",13,4));
+        modelData.setUsers(users);
+
+    }
+}
