6a10bca142eebe609109e11c36ed0b2a2a163b41
nikita <nikitachertkov@gmail.com>
2022-10-11 15:43:16 +0500

    ArrayStorage modified

--- baseJavaTasks/src/com/webapp/storage/ArrayStorage.java
+++ baseJavaTasks/src/com/webapp/storage/ArrayStorage.java
@@ -8,9 +8,6 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage{
-    private final int STORAGE_LIMIT = 10000;
-    private final Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size = 0;
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
