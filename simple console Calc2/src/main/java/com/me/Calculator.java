ea30399c40bd1ee43179d7ff32839fb5a7982393
nikita <nikitachertkov@gmail.com>
2022-10-26 11:09:30 +0500

    interface and methods

---
+++ simple console Calc2/src/main/java/com/me/Calculator.java
@@ -0,0 +1,25 @@
+package com.me;
+
+public interface Calculator {
+
+    void addition();
+
+    void subtraction();
+
+    void multiply();
+
+    void divide();
+
+    void getFirstNumber();
+
+    void getSecondNumber();
+
+    void getThirdNumber();
+
+    void storeHistory(); //записывать все действия в стрингбилдер и потом выводить на экран
+
+    // public double Koren(double a)
+    //        {
+    //            return Math.Sqrt(a);
+
+}
