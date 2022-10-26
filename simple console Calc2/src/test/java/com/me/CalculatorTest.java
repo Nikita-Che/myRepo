93241f66ba675e88f586942f4a131eedf5daf007
nikita <nikitachertkov@gmail.com>
2022-10-26 17:31:44 +0500

    test push

---
+++ simple console Calc2/src/test/java/com/me/CalculatorTest.java
@@ -0,0 +1,34 @@
+package com.me;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testAddition() {
+    }
+
+    public void testSubtraction() {
+    }
+
+    public void testMultiply() {
+    }
+
+    public void testDivide() {
+    }
+
+    public void testGetFirstNumber() {
+    }
+
+    public void testGetSecondNumber() {
+    }
+
+    public void testGetThirdNumber() {
+    }
+
+    public void testStoreHistory() {
+    }
+}
