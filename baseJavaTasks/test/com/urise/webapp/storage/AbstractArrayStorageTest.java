1e636e6d463e73afb837d24f2b95bd050b8bb634
nikita <nikitachertkov@gmail.com>
2022-11-04 11:14:04 +0500

    all tests pased!!!

--- baseJavaTasks/test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ baseJavaTasks/test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -12,6 +12,7 @@ public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
 
     protected AbstractArrayStorageTest(Storage storage) {
         super(storage);
+        this.storage = storage;
     }
 
     @Test
