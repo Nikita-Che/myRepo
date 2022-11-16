fc22a6c64d036e41eb36231ff9f11affe583295e
nikita <nikitachertkov@gmail.com>
2022-11-16 10:10:10 +0500

    hw 7 ready3

--- baseJavaTasks/src/com/urise/webapp/model/TextSection.java
+++ baseJavaTasks/src/com/urise/webapp/model/TextSection.java
@@ -1,18 +1,31 @@
 package com.urise.webapp.model;
 
+import java.util.Objects;
+
 public class TextSection extends AbstractSection { //PERSONAL, OBJECTIVE
-    String content;
+    private final String content;
 
     public TextSection(String content) {
-        this.content=content;
+        this.content = content;
     }
 
     public String getContent() {
         return content;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TextSection that = (TextSection) o;
+
+        return Objects.equals(content, that.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return content != null ? content.hashCode() : 0;
     }
 
     @Override
