50ae2034ac6692b304861f5a1246f2e78663a11d
nikita <nikitachertkov@gmail.com>
2022-11-21 17:29:17 +0500

    hw 8 preparing4

--- baseJavaTasks/src/com/urise/webapp/MainFile.java
+++ baseJavaTasks/src/com/urise/webapp/MainFile.java
@@ -1,20 +1,40 @@
 package com.urise.webapp;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Objects;
 
 public class MainFile {
-    public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Users\\nikita\\Desktop\\GitHub\\basejava\\.gitignore");
-        System.out.println(file.getCanonicalFile());
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\nikita\\Desktop\\GitHub\\basejava\\.gitignore";
+        File file = new File(filePath);
+        try {
+            System.out.println(file.getCanonicalFile());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         File dir = new File("C:\\Users\\nikita\\Desktop\\GitHub\\basejava\\src\\com\\urise\\webapp");
         System.out.println(dir.isDirectory());
         for (String name : Objects.requireNonNull(dir.list())) {
             System.out.println(name);
         }
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(filePath);
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
 
-        
     }
 }
