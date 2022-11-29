456e3b9b63fea92b184655cde92066e44fbcbb25
nikita <nikitachertkov@gmail.com>
2022-11-29 16:54:19 +0500

    xml2

--- baseJavaTasks/test/com/urise/webapp/storage/ObjectStreamFileStorageTest.java
+++
@@ -1,9 +0,0 @@
-package com.urise.webapp.storage;
-
-import com.urise.webapp.storage.strategie.ObjectStreamSerializer;
-
-public class ObjectStreamFileStorageTest extends AbstractStorageTest {
-    public ObjectStreamFileStorageTest() {
-        super(new FileStorage(STORAGE_DIR, new ObjectStreamSerializer()));
-    }
-}
